2bd3146e54e739e8688d864cd37bec8b7f89726d
Daikone <eduardoarzolay@gmail.com>
2023-11-11 14:48:13 -0400

    feat: added InputWithIcon component :D and added email.svg
    created InputWithIcon.svelte component
    added email.svg icon beforehand, it'll be used in future feats (log in and sign in)
    created input-with-icon.test.ts with component tests for InputWithIcon
    created html-input-type-attribute.type.ts, where the HTMLInputTypeAttribute type is defined

---
+++ src/lib/input-with-icon/InputWithIcon.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import type { HTMLInputTypeAttribute } from '../../types/html-input-type-attribute.type'
+
+	export let label = 'Default label'
+	export let placeholder = 'Default placeholder'
+	export let type: HTMLInputTypeAttribute
+	export let value: string | number = ''
+  export let src: string
+  export let className = ''
+</script>
+
+<label class={"flex max-w-fit border-4 border-gray-300 rounded-xl " + className}>
+	<aside class="flex items-center justify-center py-4">
+		<div class="flex items-center justify-center border-r-2 px-3 pr-2 border-gray-300 h-full">
+			<img class="w-[40px] aspect-[15/11]" alt="" {src} />
+		</div>
+	</aside>
+	<div class="flex flex-col p-4">
+		<span class="text-xs">{label}</span>
+		{#if type === 'text'}
+			<input type="text" {placeholder} bind:value class="text-sm" />
+		{:else if type === 'number'}
+			<input type="number" {placeholder} bind:value class="text-sm" />
+		{:else if type === 'email'}
+			<input type="email" {placeholder} bind:value class="text-sm" />
+		{:else if type === 'password'}
+			<input type="password" {placeholder} bind:value class="text-sm" />
+		{:else if type === 'tel'}
+			<input type="tel" {placeholder} bind:value class="text-sm" />
+		{/if}
+	</div>
+</label>
