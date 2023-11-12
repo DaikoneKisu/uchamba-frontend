9aac5de913bf275bed7415a865ca1c057ba95437
NØRA <87492801+Noralgorithm@users.noreply.github.com>
2023-11-11 20:23:46 -0400

    chore: tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				'ucab-yellow': '#FFD200',
+				'ucab-blue': '#41B4E5',
+				'ucab-green': '#007934',
+				'ucab-black': '#000000',
+				'brand-opaque-black': '#414042',
+				'brand-p-black': '#343434',
+				'brand-h1-black': '#343434',
+				'brand-h2-black': '#4D4D4D',
+				'brand-white': '#FFFFFF'
+			}
+		}
 	},
 	plugins: []
 }
