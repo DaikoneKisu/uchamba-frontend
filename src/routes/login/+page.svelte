e863c3fb21cfccd4615df1f65db48aca9e9f4512
Daikone <eduardoarzolay@gmail.com>
2023-11-12 22:32:17 -0400

    feat: add Login Form component to /login

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,8 @@
+<script lang=ts>
+  import Form from "./Form.svelte";
+
+  let email: string
+  let password: string
+</script>
+
+<Form bind:email={email} bind:password={password} />
---
+++ src/routes/login/Form.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import InputWithIcon from '$lib/input-with-icon/InputWithIcon.svelte'
+
+	import emailIcon from '$lib/icons/email.svg'
+	import keyIcon from '$lib/icons/key.svg'
+
+	export let email: string
+	export let password: string
+</script>
+
+<form 
+  aria-label="form" 
+  class="flex flex-col gap-5 min-w-[400px]"
+>
+	<InputWithIcon
+		type="email"
+		placeholder="johndalton@est.ucab.edu.ve"
+		label="Correo electrónico"
+		bind:value={email}
+		src={emailIcon}
+	/>
+	<InputWithIcon
+		type="password"
+		placeholder=""
+		label="Contraseña"
+		bind:value={password}
+		src={keyIcon}
+	/>
+	<button
+		type="submit"
+		aria-label="button"
+		class="mt-5 bg-ucab-green text-brand-white rounded-[10px] w-[330px] h-[64px] m-auto"
+	>
+    Continuar
+  </button>
+</form>
