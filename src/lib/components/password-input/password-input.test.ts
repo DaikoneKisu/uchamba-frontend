d7ffa12e933508f1534427383fefec8a5989a133
Daikone <eduardoarzolay@gmail.com>
2023-11-17 18:53:01 -0400

    test: add missing prop value to PasswordInput component rendered in password-input.test.ts

--- src/lib/components/password-input/password-input.test.ts
+++ src/lib/components/password-input/password-input.test.ts
@@ -6,7 +6,8 @@ import PasswordInput__SvelteComponent_ from './PasswordInput.svelte'
 
 describe('Password Input Component', () => {
 	test('it should render', () => {
-		render(PasswordInput__SvelteComponent_, { label: 'password' })
+    const value = ''
+		render(PasswordInput__SvelteComponent_, { label: 'password', value })
 
 		const input = screen.getByLabelText('password')
     
