a19ca1e4cd913aff9fae64d8159bc074cb0218ed
Daikone <eduardoarzolay@gmail.com>
2023-11-17 20:43:00 -0400

    fix: re-improve input with icon styles 'cuz improvement got deleted in a previous commit

--- src/lib/components/input-with-icon/InputWithIcon.svelte
+++ src/lib/components/input-with-icon/InputWithIcon.svelte
@@ -1,16 +1,16 @@
 <script lang="ts">
 	import type { HTMLInputTypeAttribute } from '../../../types/html-input-type-attribute.type'
 
-  export let name: string
-	export let label: string = "Default label"
+	export let name: string
+	export let label: string = 'Default label'
 	export let placeholder = 'Default placeholder'
 	export let type: HTMLInputTypeAttribute
 	export let value: string | number = ''
-  export let src: string
-  export let className = ''
+	export let src: string
+	export let className = ''
 </script>
 
-<label class={"flex border-4 border-gray-300 rounded-xl " + className}>
+<label class={'flex border-4 border-gray-300 rounded-xl bg-brand-white ' + className}>
 	<aside class="flex items-center justify-center py-4">
 		<div class="flex items-center justify-center border-r-2 px-3 pr-2 border-gray-300 h-full">
 			<img class="w-[40px] aspect-[15/11]" alt="" {src} />
@@ -19,13 +19,13 @@
 	<div class="flex flex-col p-4 w-full">
 		<span class="text-xs">{label}</span>
 		{#if type === 'text'}
-			<input {name} type="text" {placeholder} bind:value class="text-sm" />
+			<input {name} type="text" {placeholder} bind:value class="text-sm focus:outline-none focus:border-0 focus:shadow-none" />
 		{:else if type === 'number'}
-			<input {name} type="number" {placeholder} bind:value class="text-sm" />
+			<input {name} type="number" {placeholder} bind:value class="text-sm focus:outline-none focus:border-0 focus:shadow-none" />
 		{:else if type === 'email'}
-			<input {name} type="email" {placeholder} bind:value class="text-sm" />
+			<input {name} type="email" {placeholder} bind:value class="text-sm focus:outline-none focus:border-0 focus:shadow-none" />
 		{:else if type === 'tel'}
-			<input {name} type="tel" {placeholder} bind:value class="text-sm" />
+			<input {name} type="tel" {placeholder} bind:value class="text-sm focus:outline-none focus:border-0 focus:shadow-none" />
 		{/if}
 	</div>
 </label>
