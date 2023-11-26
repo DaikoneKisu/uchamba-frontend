e184a4de9590685e115550ff7881aa0454f43da3
NØRA <87492801+Noralgorithm@users.noreply.github.com>
2023-11-26 12:30:58 -0400

    fix: change save button and chips hover color

--- src/routes/profile/education/SaveModalFooter.svelte
+++ src/routes/profile/education/SaveModalFooter.svelte
@@ -7,7 +7,7 @@
 <footer class="w-full flex justify-end pr-10 border-t-2 border-[#f3f3f3] pt-6 pb-3">
 	<button
 		on:click={handleSave}
-		class="flex items-center justify-center gap-2 max-w-[181px] w-full h-[46px] rounded-[10px] bg-ucab-green text-brand-white shadow-xl hover:shadow-emerald-200 hover:shadow-lg hover:bg-emerald-500 transition-all duration-200"
+		class="flex items-center justify-center gap-2 max-w-[181px] w-full h-[46px] rounded-[10px] bg-ucab-green text-brand-white shadow-xl hover:shadow-emerald-200 hover:shadow-lg hover:bg-green-600 transition-all duration-200"
 	>
 		<img src={saveIcon} alt="" />
 		<span>Guardar</span>
