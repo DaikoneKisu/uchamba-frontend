1654129da1123b4685b9d25117cb3fc84c9b8efd
yeni97 <99105596+yeni97@users.noreply.github.com>
2023-11-26 17:05:15 -0400

    feat: create academic experience

--- src/routes/profile/experience/+page.server.ts
+++ src/routes/profile/experience/+page.server.ts
@@ -1,14 +1,42 @@
-import type { Experience } from '../../../types/experience-data.type'
 
 export function load() {
-	const experience: Experience = {
-		workExperience: [
-			'Software Engineer at Google (2018-present)',
-			'Software Engineer at Facebook (2016-2018)',
-			'Software Engineer at Amazon (2014-2016)'
+	const experience = {
+		workExperience1: [
+			{
+				"workExpId": 1,
+				"organizationName": "Offside CA",
+				"jobTitle": "Diseñadora UX/UI y Front-end developer",
+				"description": "Como líder del equipo de UX/UI en la sección de mercado de nuestra plataforma, tuve el privilegio de encabezar iniciativas clave para mejorar la experiencia del usuario y la interfaz de usuario. ",
+				"address": "Venezuela - Puerto Ordaz",
+				"entryDate": "25/11/2023 - 09:35 AM",
+				"departureDate": "25/11/2023 - 09:45 AM",
+				"createdAt": "25/11/2023 - 09:35 AM"
+			}
 		],
-		technologies: ['Spring Boot', 'React', 'Angular', 'AWS', 'Azure']
-	}
-
-	return experience
+		workExperience2: [
+			{
+				"workExpId": 1,
+				"organizationName": "Offside CA",
+				"jobTitle": "Armador / Organizador de Comandas",
+				"description": "Lider del equipo de UX/UI en la sección de mercado de la plataforma,además lideré la célula del team WebApp con 5 integrantes bajo la metodología de Scrum",
+				"address": "Venezuela - Puerto Ordaz",
+				"entryDate": "25/11/2023 - 09:35 AM",
+				"departureDate": "25/11/2023 - 09:45 AM",
+				"createdAt": "25/11/2023 - 09:35 AM"
+			}
+		],
+		workExperience3: [
+			{
+				"workExpId": 1,
+				"organizationName": "Offside CA",
+				"jobTitle": "Armador / Organizador de Comandas",
+				"description": "Lider del equipo de UX/UI en la sección de mercado de la plataforma,además lideré la célula del team WebApp con 5 integrantes bajo la metodología de Scrum",
+				"address": "Venezuela - Puerto Ordaz",
+				"entryDate": "26/11/2023 - 09:00 AM",
+				"departureDate": "25/11/2023 - 09:45 AM",
+				"createdAt": "26/11/2023 - 10:35 AM"
+			}
+		],
+	};
+	return experience;
 }
---
+++ src/routes/profile/experience/AcademicExperience.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+    import Add from "$lib/components/profile/add/Add.svelte"
+    import pencilIcon from '$lib/icons/pencil.svg'
+    import deleteIcon from '$lib/icons/delete.svg'
+    
+    export let business: {
+          "workExpId": number,
+          "organizationName": string,
+          "jobTitle": string,
+          "description": string,
+          "address": string,
+          "entryDate": string,
+          "departureDate": string,
+          "createdAt": string,
+          "showInformation": boolean
+        }[]
+</script>
+
+<article class="bg-brand-white flex-col w-full">
+    <header>
+        <div class="flex justify-between w-full">
+            <h2>Experiencia Laboral</h2>
+            <Add clickHandler={() => {}} />
+        </div>
+        <div class="h-1 bg-ucab-blue w-full mt-2" />
+    </header>
+
+    <ul class="flex flex-col gap-8 mt-6">
+        {#each business as busines}
+        <li class="flex flex-col gap-2">
+            <div class="flex justify-between items-center">
+                <h3 class="text-2xl font-poppins">{busines.organizationName}</h3>
+                <div class="flex gap-6">
+                    <button>
+                        <img src={pencilIcon} alt="Editar estudio" />
+                    </button>
+                    <button>
+                        <img src={deleteIcon} alt="Eliminar estudio" />
+                    </button>
+                </div>
+            </div>
+            
+            <p class="font-bold font-open-sans text-ucab-black">{busines.jobTitle}</p>
+            <p class="font-open-sans text-brand-p-black">{busines.description}</p>
+            <p>{busines.address}</p>
+            
+            {#if busines.showInformation}
+                <p>Fecha de entrada: {busines.entryDate}</p>
+                <p>Fecha de salida: {busines.departureDate}</p>
+            {/if}
+            
+            <div class="flex justify-start items-center mt-2">
+                <button class="bg-white text-ucab-green p-2 rounded">
+                    Ver Información
+                </button>
+            </div>
+        </li>
+        {/each}
+    </ul>
+</article>
