19baca09ad75e0e1c7be583da06fbabe2304a12f
NØRA <87492801+Noralgorithm@users.noreply.github.com>
2023-12-05 20:03:57 -0400

    feat: increment internal space in the button

--- src/lib/components/profile/add/Add.svelte
+++ src/lib/components/profile/add/Add.svelte
@@ -4,7 +4,7 @@
 	export let clickHandler: () => void
 </script>
 
-<button on:click={clickHandler} class="flex items-center justify-center px-4 py-1 gap-1 rounded-[10px] bg-ucab-green text-brand-white shadow-lg hover:shadow-emerald-200 hover:shadow-lg hover:bg-green-600 transition-all duration-200 ">
+<button on:click={clickHandler} class="flex items-center justify-center pl-5 pr-6 py-1 gap-2 rounded-[10px] bg-ucab-green text-brand-white shadow-lg hover:shadow-emerald-200 hover:shadow-lg hover:bg-green-600 transition-all duration-200 ">
 	<img src={addIcon} alt="Añadir enlace de interés" />
 	<p class="text-[20px] font-medium font-poppins">Añadir</p>
 </button>
