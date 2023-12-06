b2f64373e843e7875042a5d89f47d767991910b7
NØRA <87492801+Noralgorithm@users.noreply.github.com>
2023-12-06 18:32:07 -0400

    fix: pass type prop to inputs in details modal

--- src/routes/profile/experience/ExperienceDetailsModal.svelte
+++ src/routes/profile/experience/ExperienceDetailsModal.svelte
@@ -2,18 +2,18 @@
 	import Modal from '$lib/components/profile/modal/Modal.svelte'
 	import Input from '$lib/components/input/Input.svelte'
 	import busines from '$lib/icons/business.svg'
-    import SaveModalFooter from './SaveModalFooters.svelte'	
-    import { invalidateAll } from '$app/navigation'
+	import SaveModalFooter from './SaveModalFooters.svelte'
+	import { invalidateAll } from '$app/navigation'
 
 	export let openedModal = false
 
 	let formData = {
-				"organizationName":'',
-				"jobTitle":'',
-				"description": '',
-				"address": '',
-				"entryDate":'',
-				"departureDate": '',
+		organizationName: '',
+		jobTitle: '',
+		description: '',
+		address: '',
+		entryDate: '',
+		departureDate: ''
 	}
 
 	function save() {
@@ -35,33 +35,39 @@
 	<form slot="body" class="w-full flex pl-6 py-12 justify-between">
 		<div class="flex flex-col w-full gap-12">
 			<Input
+				type="text"
 				label="Organizacion"
 				placeholder="Ingrese el instituto o universidad"
 				bind:value={formData.organizationName}
 			/>
 			<Input
+				type="text"
 				label="Fecha de entrada"
 				placeholder="dd/mm/aaaa"
 				bind:value={formData.entryDate}
 			/>
-            <Input
-				label="Rol"
-				placeholder="Ingrese el rol"
-				bind:value={formData.jobTitle}
-			/>
+			<Input type="text" label="Rol" placeholder="Ingrese el rol" bind:value={formData.jobTitle} />
 			<Input
+				type="text"
 				label="Descripcion"
 				placeholder="Ingrese una breve descripcion"
 				bind:value={formData.description}
 			/>
-		
 		</div>
-			
 
-		
 		<div class="flex flex-col w-full items-center gap-12">
-			<Input label="Direccion" placeholder="Ingrese la direccion" bind:value={formData.address} />
-			<Input label="Fecha de salida (opcional)" placeholder="dd/mm/aaaa" bind:value={formData.departureDate} />
+			<Input
+				type="text"
+				label="Direccion"
+				placeholder="Ingrese la direccion"
+				bind:value={formData.address}
+			/>
+			<Input
+				type="text"
+				label="Fecha de salida (opcional)"
+				placeholder="dd/mm/aaaa"
+				bind:value={formData.departureDate}
+			/>
 		</div>
 	</form>
 
