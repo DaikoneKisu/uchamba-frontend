beb3eb99bff3b8548d32aefa97ed6599a4807202
NØRA <87492801+Noralgorithm@users.noreply.github.com>
2023-12-06 18:30:21 -0400

    fix: pass type prop to inputs

--- src/routes/profile/experience/ExperienceCreationModal.svelte
+++ src/routes/profile/experience/ExperienceCreationModal.svelte
@@ -35,21 +35,34 @@
 	<form slot="body" class="w-full flex pl-6 py-12 justify-between">
 		<div class="flex flex-col w-full gap-12">
 			<Input
+				type="text"
 				label="Organizacion"
 				placeholder="Ingrese el instituto o universidad"
 				bind:value={formData.organizationName}
 			/>
-			<Input label="Fecha de entrada" placeholder="dd/mm/aaaa" bind:value={formData.entryDate} />
-			<Input label="Rol" placeholder="Ingrese el rol" bind:value={formData.jobTitle} />
 			<Input
+				type="text"
+				label="Fecha de entrada"
+				placeholder="dd/mm/aaaa"
+				bind:value={formData.entryDate}
+			/>
+			<Input type="text" label="Rol" placeholder="Ingrese el rol" bind:value={formData.jobTitle} />
+			<Input
+				type="text"
 				label="Descripcion"
 				placeholder="Ingrese una breve descripcion"
 				bind:value={formData.description}
 			/>
 		</div>
 		<div class="flex flex-col w-full items-center gap-12">
-			<Input label="Direccion" placeholder="Ingrese la direccion" bind:value={formData.address} />
 			<Input
+				type="text"
+				label="Direccion"
+				placeholder="Ingrese la direccion"
+				bind:value={formData.address}
+			/>
+			<Input
+				type="text"
 				label="Fecha de salida (opcional)"
 				placeholder="dd/mm/aaaa"
 				bind:value={formData.departureDate}
