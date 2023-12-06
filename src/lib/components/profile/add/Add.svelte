ad4f5351834bfe488d6102f74ffa268c28e15ed9
NØRA <87492801+Noralgorithm@users.noreply.github.com>
2023-12-05 20:02:15 -0400

    feat: add button styles to make it more similar to the figma version

--- src/lib/components/profile/add/Add.svelte
+++ src/lib/components/profile/add/Add.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
-	import addIcon from '$lib/icons/add.svg'
+	import addIcon from '$lib/icons/add-white.svg'
 
 	export let clickHandler: () => void
 </script>
 
-<button on:click={clickHandler} class="flex items-end gap-1">
+<button on:click={clickHandler} class="flex items-center justify-center px-4 py-1 gap-1 rounded-[10px] bg-ucab-green text-brand-white shadow-lg hover:shadow-emerald-200 hover:shadow-lg hover:bg-green-600 transition-all duration-200 ">
 	<img src={addIcon} alt="Añadir enlace de interés" />
-	<p class="text-xl font-semibold font-poppins">Añadir</p>
+	<p class="text-[20px] font-medium font-poppins">Añadir</p>
 </button>
