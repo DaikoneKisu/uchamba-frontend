f554c787ea9ee1338d677cd450d9202641105c7c
NØRA <87492801+Noralgorithm@users.noreply.github.com>
2023-12-17 15:11:24 -0400

    feat: create +page for portfolio

---
+++ src/routes/(authed)/profile/[id=profile_id]/portfolio/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import type { ProfileData } from '../../../../../types/profile-data.type'
+	import Projects from './Projects.svelte'
+
+	export let data: ProfileData
+</script>
+
+<div class="w-full flex flex-col justify-between pb-28 items-center pt-8 gap-16 px-16">
+	<Projects profileData={data} />
+</div>
