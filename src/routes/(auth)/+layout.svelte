b89668595fa2d32794f294e32f0e379f5cc237a6
Daikone <eduardoarzolay@gmail.com>
2023-12-21 01:39:35 -0400

    feat!: delete sign up feature traces
    BREAKING CHANGE: sign up related stuff (routes, files, components) no longer exist

--- src/routes/(auth)/+layout.svelte
+++ src/routes/(auth)/+layout.svelte
@@ -1,17 +1,7 @@
 <script lang="ts">
-	import { page } from '$app/stores'
 	import ucabLogo from '$lib/images/logo-ucab.webp'
 	import firstLoginImage from '$lib/images/login-image-one.png'
 	import secondLoginImage from '$lib/images/login-image-two.png'
-	import SignUpLogInSwitchButton from '$lib/components/sign-up-log-in-switch-button/SignUpLogInSwitchButton.svelte'
-
-	let chosen: 'Iniciar sesión' | 'Registrarse' = 'Iniciar sesión'
-
-	$: if ($page.url.pathname === '/sign-up') {
-		chosen = 'Registrarse'
-	} else if ($page.url.pathname === '/login') {
-		chosen = 'Iniciar sesión'
-	}
 </script>
 
 <main class="min-h-screen flex flex-col items-center justify-start gap-10 pb-24 pt-10">
@@ -19,19 +9,8 @@
 
 	<div class="flex flex-col justify-center items-center">
 		<div class="flex flex-col gap-3">
-			{#if $page.url.pathname === '/sign-up'}
-				<h1 class="text-center">¡Bienvenido!</h1>
-				<h2 class="text-center text-zinc-400">
-					A nuestro catálogo de egresados en línea
-				</h2>
-			{:else if $page.url.pathname === '/login'}
-				<h1 class="text-center">Bienvenido de regreso</h1>
-				<h2 class="text-center text-zinc-400">A nuestro catálogo de egresados en línea</h2>
-			{/if}
-		</div>
-
-		<div class="py-10">
-			<SignUpLogInSwitchButton {chosen} logInUrl="/login" signInUrl="/sign-up" />
+      <h1 class="text-center">Bienvenido de regreso</h1>
+      <h2 class="text-center text-zinc-400">A nuestro catálogo de egresados en línea</h2>
 		</div>
 
 		<div class="invisible xl:visible -z-10">
