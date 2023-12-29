1d238dfc071f19f48474c8cfc25b5de7a0b660e5
Jose Andres <87492801+Noralgorithm@users.noreply.github.com>
2023-12-29 16:11:44 -0400

    style: line between imports and sort
    Co-authored-by: Daikone <eduardoarzolay@gmail.com>

--- src/routes/profile/[id=profile_id]/education/AcademicTrainingEditionModal.svelte
+++ src/routes/profile/[id=profile_id]/education/AcademicTrainingEditionModal.svelte
@@ -1,12 +1,15 @@
 <script lang="ts">
-  import graduationCapIcon from '$lib/icons/graduation-cap.svg'
+  import { ValidationError } from 'yup'
+  import { invalidateAll } from '$app/navigation'
+  
+  import type { PersonalStudy } from '$lib/types/profile-data.type'
+  
   import Modal from '$lib/components/profile/modal/Modal.svelte'
   import Input from '$lib/components/input/Input.svelte'
   import SaveModalFooter from '$lib/components/profile/modal/SaveModalFooter.svelte'
-  import type { PersonalStudy } from '$lib/types/profile-data.type'
-  import { invalidateAll } from '$app/navigation'
   import { validateAcademicTraining } from '$lib/profile/education/validate-academic-training'
-  import { ValidationError } from 'yup'
+
+  import graduationCapIcon from '$lib/icons/graduation-cap.svg'
 
   export let isOpen: boolean
 
