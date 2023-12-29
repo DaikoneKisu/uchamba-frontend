0461b6857d8e851592d715875437c80b5e374b01
Jose Andres <87492801+Noralgorithm@users.noreply.github.com>
2023-12-29 16:14:24 -0400

    style: line between imports and sort
    Co-authored-by: Daikone <eduardoarzolay@gmail.com>

--- src/routes/profile/[id=profile_id]/education/HardSkillCreationModal.svelte
+++ src/routes/profile/[id=profile_id]/education/HardSkillCreationModal.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
-  import Modal from '$lib/components/profile/modal/Modal.svelte'
-  import learningIcon from '$lib/icons/learning.svg'
-  import SaveModalFooter from '$lib/components/profile/modal/SaveModalFooter.svelte'
-  import { invalidateAll } from '$app/navigation'
-  import Chip from '$lib/components/profile/chip/Chip.svelte'
   import { flip } from 'svelte/animate'
   import { fade } from 'svelte/transition'
+  import { invalidateAll } from '$app/navigation'
+  
+  import Modal from '$lib/components/profile/modal/Modal.svelte'
+  import SaveModalFooter from '$lib/components/profile/modal/SaveModalFooter.svelte'
+  import Chip from '$lib/components/profile/chip/Chip.svelte'
+  
+  import learningIcon from '$lib/icons/learning.svg'
 
   export let openedModal = false
 
