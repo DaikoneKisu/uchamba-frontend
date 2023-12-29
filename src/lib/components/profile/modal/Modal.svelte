f435c26758a7ea1201520ebdd679dca6f8b8c15c
Jose Andres <87492801+Noralgorithm@users.noreply.github.com>
2023-12-29 06:04:24 -0400

    style: line between imports
    Co-authored-by: Daikone <eduardoarzolay@gmail.com>

--- src/lib/components/profile/modal/Modal.svelte
+++ src/lib/components/profile/modal/Modal.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
-  import cancelIcon from '$lib/icons/cancel-black.svg'
   import { fade, scale } from 'svelte/transition'
+  
+  import cancelIcon from '$lib/icons/cancel-black.svg'
 
   export let isOpen: Boolean
   export let title = ''
