cc12e5969bb7cee9c0f3912616aa49a30c6bd9a4
Jose Andres <87492801+Noralgorithm@users.noreply.github.com>
2023-12-29 06:09:46 -0400

    refactor: use shorthand transition instead of separated in and out attributes
    Co-authored-by: Daikone <eduardoarzolay@gmail.com>

--- src/lib/components/profile/modal/Modal.svelte
+++ src/lib/components/profile/modal/Modal.svelte
@@ -15,8 +15,7 @@
 
 {#if isOpen}
   <div
-    in:fade={{ duration: 100 }}
-    out:fade={{ duration: 100 }}
+    transition:fade={{ duration: 100 }}
     class="fixed left-0 top-0 flex h-full w-full items-center justify-center bg-black bg-opacity-50 z-10"
   >
     <div
