6faf11ad19965a456bc9d581f57ba525a23ee591
yeni97 <99105596+yeni97@users.noreply.github.com>
2024-01-03 11:39:17 -0400

    fix: yellow bars in navigation bar

--- src/lib/components/navegation.svelte
+++ src/lib/components/navegation.svelte
@@ -1,49 +1,66 @@
 <script lang="ts">
-	import { onMount } from 'svelte'
+	import { page } from '$app/stores'
+	import { crossfade } from 'svelte/transition'
 
-	let currentPath: string = window.location.pathname
+	let currentRoute: 'prueba1' | 'catalogue' | 'prueba2' = 'prueba1'
 
-	onMount(() => {
-		window.addEventListener('popstate', () => {
-			currentPath = window.location.pathname
-		})
+	$: currentRoute = $page.url.pathname.split('/').pop() as 'prueba1' | 'catalogue' | 'prueba2'
+
+	const [send, receive] = crossfade({
+		duration: (d) => Math.sqrt(d * 200)
 	})
 </script>
 
 <header class="w-full">
 	<nav class="flex justify-between items-center bg-white p-2 rounded-[10px] mb-0">
-		<a
-			href="/prueba1"
-			class="relative w-[275px] h-[37px] text-center text-black text-[15px] font-bold font-['Source Sans Pro'] leading-[37.50px]"
+		<ul
+			class="w-full text-lg text-center max-w-xl flex justify-between [&>li]:min-w-[150px] [&>li>a]:py-5 [&>li>a]:text-center"
 		>
-			Página Principal
-			{#if currentPath === '/prueba1'}
-				<div
-					class="w-[35.03px] h-[2px] bg-yellow-400 absolute bottom-0 left-[50%] transform -translate-x-1/2"
-				/>
-			{/if}
-		</a>
-		<a
-			href="/catalogue"
-			class="relative w-[275px] h-[37px] text-center text-black text-[15px] font-bold font-['Source Sans Pro'] leading-[37.50px]"
-		>
-			Catálogo
-			{#if currentPath === '/catalogue'}
-				<div
-					class="w-[35.03px] h-[2px] bg-yellow-400 absolute bottom-0 left-[50%] transform -translate-x-1/2"
-				/>
-			{/if}
-		</a>
-		<a
-			href="/prueba2"
-			class="relative w-[275px] h-[37px] text-center text-black text-[15px] font-bold font-['Source Sans Pro'] leading-[37.50px]"
-		>
-			Iniciar Sesión
-			{#if currentPath === '/prueba2'}
-				<div
-					class="w-[35.03px] h-[2px] bg-yellow-400 absolute bottom-0 left-[50%] transform -translate-x-1/2"
-				/>
-			{/if}
-		</a>
+			<li class="min-w-[150px] ml-32">
+				<a
+					href="/prueba1"
+					class="relative w-[275px] h-[37px] text-center text-black text-[15px] font-bold font-['Source Sans Pro'] leading-[37s.50px]"
+				>
+					Página Principal
+					{#if currentRoute === 'prueba1'}
+						<div
+							in:receive={{ key: 'line' }}
+							out:send={{ key: 'line' }}
+							class="h-[2px] w-10 bg-yellow-400 absolute translate-y-0 ml-12"
+						/>
+					{/if}
+				</a>
+			</li>
+			<li class="min-w-[150px] ml-72"> 
+				<a
+					href="/catalogue"
+					class="relative w-[275px] h-[37px] text-center text-black text-[15px] font-bold font-['Source Sans Pro'] leading-[37s.50px]"
+				>
+					Catálogo
+					{#if currentRoute === 'catalogue'}
+						<div
+							in:receive={{ key: 'line' }}
+							out:send={{ key: 'line' }}
+							class="h-[2px] w-10 bg-yellow-400 absolute translate-y-0 ml-12"
+						/>
+					{/if}
+				</a>
+			</li>
+			<li class="min-w-[150px] ml-48">
+				<a
+					href="/prueba2"
+					class="relative w-[275px] h-[37px] text-center text-black text-[15px] font-bold font-['Source Sans Pro'] leading-[37s.50px]"
+				>
+					Iniciar Sesión
+					{#if currentRoute === 'prueba2'}
+						<div
+							in:receive={{ key: 'line' }}
+							out:send={{ key: 'line' }}
+							class="h-[2px] w-10 bg-yellow-400 absolute translate-y-0 ml-12"
+						/>
+					{/if}
+				</a>
+			</li>
+		</ul>
 	</nav>
 </header>
