b229c6be886863f93fd098ecbec1e19a5cb70fde
NØRA <87492801+Noralgorithm@users.noreply.github.com>
2024-01-04 16:32:07 -0400

    fix: careers ids

--- src/lib/profile/careers/careers.store.ts
+++ src/lib/profile/careers/careers.store.ts
@@ -1,13 +1,13 @@
 import { readable } from 'svelte/store'
 
 export const careers = readable<Record<number, string>>({
-  1: 'Ingeniería Informatica',
-  2: 'Ingeniería Civil',
-  3: 'Ingeniería Industrial',
-  4: 'Relaciones Industriales',
+  1: 'Administración de Empresas',
+  2: 'Comunicación Social',
+  3: 'Contaduría Pública',
+  4: 'Derecho',
   5: 'Educación',
-  6: 'Comunicación Social',
-  7: 'Derecho',
-  8: 'Administración de Empresas',
-  9: 'Contaduría Pública'
+  6: 'Ingeniería Civil',
+  7: 'Ingeniería Industrial',
+  8: 'Ingeniería Informática',
+  9: 'Relaciones Industriales'
 })
