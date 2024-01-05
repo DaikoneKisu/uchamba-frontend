d525ac026c0f75fa3cec3578931db19d37140a0c
NØRA <87492801+Noralgorithm@users.noreply.github.com>
2024-01-05 18:07:52 -0400

    feat: description limit validation

--- src/lib/profile/experience/validate-academic-experience.ts
+++ src/lib/profile/experience/validate-academic-experience.ts
@@ -3,8 +3,10 @@ import { object, string, boolean, date, ref } from 'yup'
 const schema = object({
   organizationName: string().required('Campo requerido'),
   jobTitle: string().required('Campo requerido'),
-  description: string().required('Campo requerido'),
-  entryDate: date().transform((curr: Date, orig) => (orig === '' ? null : curr)).required('Campo requerido'),
+  description: string().max(596, 'Se superó el límite de caracteres').required('Campo requerido'),
+  entryDate: date()
+    .transform((curr: Date, orig) => (orig === '' ? null : curr))
+    .required('Campo requerido'),
   departureDate: date()
     .nullable()
     .transform((curr: Date, orig) => (orig === '' ? null : curr))
@@ -12,7 +14,6 @@ const schema = object({
       is: (entryDate: Date) => Boolean(entryDate),
       then: (s) => s.min(ref('entryDate'), 'La fecha de salida debe ser posterior a la de entrada')
     }),
-
   freelancer: boolean().required('Campo requerido'),
   country: string().when('freelancer', {
     is: false,
