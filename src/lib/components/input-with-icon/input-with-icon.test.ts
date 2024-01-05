ce6d179974bd337db06241112317a3c8455d210f
Daikone <eduardoarzolay@gmail.com>
2024-01-05 19:37:58 -0400

    test: fix tests due to missing label prop

--- src/lib/components/input-with-icon/input-with-icon.test.ts
+++ src/lib/components/input-with-icon/input-with-icon.test.ts
@@ -18,6 +18,7 @@ describe('Input Component', () => {
 		const initialValue = 'test-'
 
 		render(InputWithIcon__SvelteComponent_, {
+      label: '',
 			type: 'text',
 			value: initialValue,
 			placeholder: 'test-placeholder',
@@ -60,6 +61,7 @@ describe('Input Component', () => {
 
 	test('it should render an icon', () => {
 		render(InputWithIcon__SvelteComponent_, {
+      label: '',
 			type: 'text',
 			src: email,
       name: 'test-name'
