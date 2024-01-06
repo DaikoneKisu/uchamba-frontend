2d32e332bb5afba0aa2230f7affcf1b5a9441048
Chocopaula <mariapforero2003@gmail.com>
2024-01-06 02:35:19 -0400

    feat: logical layout for displaying the component under construction

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,20 @@
 <script>
 	import '../app.css'
+	import { onMount } from 'svelte'
+	import UnderConstruction from '../lib/components/error-pages/UnderConstruction.svelte'
+
+	let isSmallScreen = false
+
+	onMount(() => {
+		isSmallScreen = window.innerWidth <= 1150
+		window.addEventListener('resize', () => {
+			isSmallScreen = window.innerWidth <= 1150
+		})
+	})
 </script>
 
-<slot />
+{#if isSmallScreen}
+	<UnderConstruction messagePrincipal="Visítanos desde una computadora" messageSecondary="La versión móvil está bajo construcción" />
+{:else}
+	<slot />
+{/if}
