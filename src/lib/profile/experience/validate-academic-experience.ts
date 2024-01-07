f1309cb1706a6c6df3c9c057245767c58f395745
NØRA <87492801+Noralgorithm@users.noreply.github.com>
2024-01-07 00:12:14 -0400

    feat: validate future dates

--- src/lib/profile/experience/validate-academic-experience.ts
+++ src/lib/profile/experience/validate-academic-experience.ts
@@ -6,13 +6,22 @@ const schema = object({
   description: string().max(596, 'Se superó el límite de caracteres').required('Campo requerido'),
   entryDate: date()
     .transform((curr: Date, orig) => (orig === '' ? null : curr))
-    .required('Campo requerido'),
+    .required('Campo requerido')
+    .test('is-not-a-future-year', 'No puede ser un año futuro', (value) => {
+      const currentYear = new Date()
+      return value <= currentYear
+    }),
   departureDate: date()
     .nullable()
     .transform((curr: Date, orig) => (orig === '' ? null : curr))
     .when('entryDate', {
       is: (entryDate: Date) => Boolean(entryDate),
       then: (s) => s.min(ref('entryDate'), 'La fecha de salida debe ser posterior a la de entrada')
+    })
+    .test('is-not-a-future-year', 'No puede ser un año futuro', (value) => {
+      const currentYear = new Date()
+      if (!value) return true
+      return value <= currentYear
     }),
   freelancer: boolean().required('Campo requerido'),
   country: string().when('freelancer', {
