714125e22f52fb6741aeacd3f96c56f500ab5b74
NØRA <87492801+Noralgorithm@users.noreply.github.com>
2024-01-06 20:44:16 -0400

    feat: loading in pagination

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,43 +1,48 @@
 <script lang="ts">
-	import '../app.css'
+  import '../app.css'
 
-	import { Toaster } from 'svelte-french-toast'
+  import { navigating } from '$app/stores'
+  import { Toaster } from 'svelte-french-toast'
 
-	import type { SuccessToastOptions } from '$lib/types/success-toast-options.type'
-	import type { ErrorToastOptions } from '$lib/types/error-toast-options.type'
+  import type { SuccessToastOptions } from '$lib/types/success-toast-options.type'
+  import type { ErrorToastOptions } from '$lib/types/error-toast-options.type'
 
-	import SuccessToast from '$lib/components/toasts/success-toast/SuccessToast.svelte'
-	import ErrorToast from '$lib/components/toasts/error-toast/ErrorToast.svelte'
-	import { toast } from '$lib/utils/toast'
-	import { successToast } from '$lib/stores/success-toast'
-	import { errorToast } from '$lib/stores/error-toast'
+  import SuccessToast from '$lib/components/toasts/success-toast/SuccessToast.svelte'
+  import ErrorToast from '$lib/components/toasts/error-toast/ErrorToast.svelte'
+  import { toast } from '$lib/utils/toast'
+  import { successToast } from '$lib/stores/success-toast'
+  import { errorToast } from '$lib/stores/error-toast'
+  import Loading from '$lib/components/loading/Loading.svelte'
 
-	const position = 'top-end'
-	const duration = Number.POSITIVE_INFINITY
-	const style = 'max-width: 500px; '
+  const position = 'top-end'
+  const duration = Number.POSITIVE_INFINITY
+  const style = 'max-width: 500px; '
 
-	$: if ($successToast) {
-		successToast._id(
-			toast<SuccessToastOptions>(SuccessToast, {
-				duration,
-				...$successToast,
-				style: style + $successToast.style,
-				position
-			})
-		)
-	}
+  $: if ($successToast) {
+    successToast._id(
+      toast<SuccessToastOptions>(SuccessToast, {
+        duration,
+        ...$successToast,
+        style: style + $successToast.style,
+        position
+      })
+    )
+  }
 
-	$: if ($errorToast) {
-		errorToast._id(
-			toast<ErrorToastOptions>(ErrorToast, {
-				duration,
-				...$errorToast,
-				style: style + $errorToast.style,
-				position
-			})
-		)
-	}
+  $: if ($errorToast) {
+    errorToast._id(
+      toast<ErrorToastOptions>(ErrorToast, {
+        duration,
+        ...$errorToast,
+        style: style + $errorToast.style,
+        position
+      })
+    )
+  }
 </script>
 
+{#if $navigating}
+  <Loading />
+{/if}
 <Toaster />
 <slot />
