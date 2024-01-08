48e6f42d5ad43295fcdda2fe6dfc3070d762d882
Daikone <eduardoarzolay@gmail.com>
2024-01-08 04:52:23 -0400

    feat: create api route /api/generate-cv

---
+++ src/routes/api/generate-cv/+servet.ts
@@ -0,0 +1,15 @@
+import { BACKEND_BASE_URL } from '$env/static/private'
+
+interface CVGenerationPayload {
+	userId: string
+	cvId: string
+}
+
+export async function POST({ request }: { request: Request }) {
+	const { userId, cvId } = (await request.json()) as CVGenerationPayload
+	const res = fetch(`${BACKEND_BASE_URL}/generar-cv/user/${userId}/cv/${cvId}`, {
+		method: 'GET'
+	})
+
+	return res
+}
