80c31a7111c1ecce6e4ec09dd71ba829d0159114
Daikone <eduardoarzolay@gmail.com>
2024-01-08 03:55:05 -0400

    refactor!: delete two components in button folder
    delete DownloadCV and ViewProfile buttons because Button replaces them

--- src/lib/components/button/DownloadCV.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts">
-</script>
-
-<button class="w-[168px] h-[43px] bg-green-700 rounded-[10px] text-white font-medium">
-	<span>Descargar CV</span>
-</button>
--- src/lib/components/button/ViewProfile.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts">
-</script>
-
-<button class="w-full max-w-[168px] h-[43px] bg-green-700 rounded-[10px] text-white font-medium">
-	<span>Ver perfil</span>
-</button>
