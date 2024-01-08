0ed521e0a9bea9c193d0cbc4403f72054c21d78b
Daikone <eduardoarzolay@gmail.com>
2024-01-08 03:52:49 -0400

    refactor!: rename buttons folder to button in components

---
+++ src/lib/components/button/ViewProfile.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+</script>
+
+<button class="w-full max-w-[168px] h-[43px] bg-green-700 rounded-[10px] text-white font-medium">
+	<span>Ver perfil</span>
+</button>
--- src/lib/components/buttons/ViewProfile.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-</script>
-
-<button
-	class="w-full max-w-[168px] h-[43px] bg-green-700 rounded-[10px] text-white font-medium ml-8"
->
-	<span>Ver perfil</span>
-</button>
