1ca523b29facd06e9727cd12f42e38c3f270236e
Daikone <eduardoarzolay@gmail.com>
2024-01-08 03:51:42 -0400

    feat: add generic Button comp with text and handleClick props

---
+++ src/lib/components/buttons/Button.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	export let text: string
+	export let handleClick: () => void = () => {}
+</script>
+
+<button
+	class="w-full max-w-[168px] h-[43px] bg-green-700 rounded-[10px] text-white"
+	on:click|preventDefault={() => handleClick()}
+>
+	{text}
+</button>
