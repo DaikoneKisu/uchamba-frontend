490bfdaf0d1cfda40ca01ad29a70f1dc368dbe6f
Daikone <eduardoarzolay@gmail.com>
2024-01-09 23:19:56 -0400

    refactor: add filter form store to share filters's form element

---
+++ src/lib/stores/filters-form.ts
@@ -0,0 +1,12 @@
+import { writable } from 'svelte/store'
+
+function createFiltersForm() {
+	const { subscribe, set } = writable<HTMLFormElement>()
+
+	return {
+		subscribe,
+		set: (form: HTMLFormElement) => set(form)
+	}
+}
+
+export const filtersForm = createFiltersForm()
