067a5bb088071fa1eb3256675b7a291c1a30ecb0
Daikone <eduardoarzolay@gmail.com>
2024-01-11 10:09:30 -0400

    fix: rerender pagination on page change to correctly get search params from url

--- src/routes/catalogue/Pagination.svelte
+++ src/routes/catalogue/Pagination.svelte
@@ -15,38 +15,43 @@
 	function getRouteUpdatingPageParam(page: number) {
 		if (browser) {
 			let currentURL = new URL(window.location.href)
+
 			return currentURL.searchParams.has('page')
 				? (() => {
 						currentURL.searchParams.set('page', page.toString())
 						return `${currentURL.pathname}${currentURL.search}`
 				  })()
-				: `${currentURL.pathname}${currentURL.search}&page=${page}`
+				: currentURL.search
+				? `${currentURL.pathname}${currentURL.search}&page=${page}`
+				: `${currentURL.pathname}?page=${page}`
 		}
-		return '/'
+		return ''
 	}
 </script>
 
 <nav class={`flex flex-wrap gap-4 ${className}`}>
-	<PageBackward
-		href={(() => getRouteUpdatingPageParam(currentPage - 1))()}
-		disabled={currentPage <= 1}
-	/>
-	<!-- 
+	{#key $page}
+		<PageBackward
+			href={(() => getRouteUpdatingPageParam(currentPage - 1))()}
+			disabled={currentPage <= 1}
+		/>
+		<!-- 
     Well, typescript-eslint's @typescript-eslint/no-unused-vars rule is disabled next line 
     because it says _ is a variable that's not being used, and while that's true, it has to 
     be there for destructuring and that's why it's named _ (common variable name for 
     variables that are not being used on purpose) 
   -->
-	<!-- eslint-disable-next-line @typescript-eslint/no-unused-vars -->
-	{#each Array(pages) as _, idx}
-		<PageNumber
-			href={(() => getRouteUpdatingPageParam(idx + 1))()}
-			pageNumber={idx + 1}
-			highlight={currentPage === idx + 1}
+		<!-- eslint-disable-next-line @typescript-eslint/no-unused-vars -->
+		{#each Array(pages) as _, idx}
+			<PageNumber
+				href={(() => getRouteUpdatingPageParam(idx + 1))()}
+				pageNumber={idx + 1}
+				highlight={currentPage === idx + 1}
+			/>
+		{/each}
+		<PageForward
+			href={(() => getRouteUpdatingPageParam(currentPage + 1))()}
+			disabled={currentPage >= pages}
 		/>
-	{/each}
-	<PageForward
-		href={(() => getRouteUpdatingPageParam(currentPage + 1))()}
-		disabled={currentPage >= pages}
-	/>
+	{/key}
 </nav>
