9d268f02ef0d7f2801f115e7f65aefb7aa846c18
Daikone <eduardoarzolay@gmail.com>
2024-01-12 01:59:51 -0400

    feat: add handleChange again

--- src/lib/components/Checkbox/Checkbox.svelte
+++ src/lib/components/Checkbox/Checkbox.svelte
@@ -5,18 +5,31 @@
 
 	export let checked = false
 	export let disabled = true
+	export let form = ''
+	export let text = ''
+	export let handleChange: (
+		e: Event & {
+			currentTarget: EventTarget & HTMLInputElement
+		}
+	) => void = () => {}
 </script>
 
 <label class="flex items-center justify-center">
 	<input
+		data-sveltekit-keepfocus
 		type="checkbox"
 		bind:checked
 		class="aspect-square w-5 min-w-[20px] appearance-none rounded-md border border-sky-600 border-opacity-20 bg-white shadow transition-colors checked:border-none checked:bg-sky-400 disabled:bg-gray-100 disabled:opacity-50 disabled:border-red-200"
 		{disabled}
+		{form}
+		on:change={handleChange}
 	/>
 	{#if checked}
 		<img in:scale src={checkmarkIcon} alt="" class="relative right-4 aspect-square w-3" />
 	{:else}
 		<div class="relative bottom-[1.4rem] left-1 aspect-square w-3" />
+	{/if}
+	{#if text}
+		<p class="inline-block ml-2">{text}</p>
 	{/if}
 </label>
