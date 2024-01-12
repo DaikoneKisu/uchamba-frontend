3ecddbda8227ec056c65de540555a0bbcbd28d33
Daikone <eduardoarzolay@gmail.com>
2024-01-12 02:37:15 -0400

    feat: add error to language filter on wrong format and show the user the error

--- src/routes/catalogue/(filters)/LanguagesDropdown.svelte
+++ src/routes/catalogue/(filters)/LanguagesDropdown.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
 	import { flip } from 'svelte/animate'
-	import { fade } from 'svelte/transition'
+	import { fade, fly } from 'svelte/transition'
 	import Chip from '$lib/components/chip/Chip.svelte'
 	import Dropdown from './Dropdown.svelte'
 	import { suggestions } from '$lib/stores/suggestions'
 	import { languageFilter } from '$lib/stores/filters'
+	import { errorToast } from '$lib/stores/error-toast'
 
 	let languages: string[]
+	let error = ''
+
+	let isPristine = true
 
 	$: choosableLanguages = $suggestions.languages.filter(
 		(lang) => !languages.some((choosenLang) => choosenLang.startsWith(lang.name + '-'))
@@ -28,16 +32,21 @@
 
 	let value = ''
 
-	let input: HTMLInputElement
-
 	function insertLanguage(language: string) {
 		if (language) {
 			const [name, proficientLevel] = language.split('-')
+			let languageFormatted = ''
 			$suggestions.languages.forEach((lang) => {
 				if (lang.name === name && lang.proficientLevel === proficientLevel) {
-					languageFilter.add(`${lang.id}-${lang.proficientLevel}`)
+					languageFormatted = `${lang.id}-${lang.proficientLevel}`
 				}
 			})
+			if (languageFormatted) {
+				languageFilter.add(languageFormatted)
+				languages = [...languages, language]
+			} else {
+				error = 'El idioma tiene el formato incorrecto, recuerde que es "Idioma-Nivel"'
+			}
 			value = ''
 		}
 	}
@@ -57,16 +66,17 @@
 	<div class="flex flex-col w-full items-center justify-center pt-2">
 		<strong class="inline-block text-green-700">Selecciona un idioma con un nivel mínimo</strong>
 		<label
-			class="flex h-[49px] w-full max-w-[330px] justify-center rounded-xl border-4 border-[#f0f0f0] bg-brand-white pr-4 mt-4"
+			class={'flex h-[49px] w-full max-w-[330px] justify-center rounded-xl border-4 border-[#f0f0f0] bg-brand-white pr-4 mt-4' +
+				(error && !isPristine ? ' border-[#D14F4F]' : '')}
 		>
 			<div class="flex h-full w-full flex-col justify-center pl-5 text-[15px]">
 				<input
-					bind:this={input}
 					list="languages-list"
 					type="text"
 					bind:value
-					placeholder="Selecciona un idioma..."
+					placeholder="Ej. Inglés-B1"
 					class="placeholder:text-brand-p-black focus:border-0 focus:shadow-none focus:outline-none bg-brand-white"
+					on:input|once={() => (isPristine = false)}
 					on:change={(e) => {
 						insertLanguage(e.currentTarget.value.trim())
 					}}
@@ -78,6 +88,13 @@
 				</datalist>
 			</div>
 		</label>
+		{#if error && !isPristine}
+			<strong in:fly={{ x: -12 }} class="ml-3 w-[80%] text-left text-[12px] text-red-600"
+				>{error}</strong
+			>
+		{:else}
+			<div class="invisible h-[18px]" aria-hidden />
+		{/if}
 	</div>
 
 	<div class="w-full">
