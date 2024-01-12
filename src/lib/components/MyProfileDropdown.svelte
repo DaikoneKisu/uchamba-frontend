0318ebe8d023294fb4b5f0a12a1a072c031cc79a
Daikone <eduardoarzolay@gmail.com>
2024-01-12 10:51:44 -0400

    changes to style in MyProfileDropdown

--- src/lib/components/MyProfileDropdown.svelte
+++ src/lib/components/MyProfileDropdown.svelte
@@ -1,37 +1,37 @@
 <script lang="ts">
-	import { slide } from 'svelte/transition'
+  import { slide } from 'svelte/transition'
 
-	import downIcon from '$lib/icons/down-button.svg'
+  import downIcon from '$lib/icons/down-button.svg'
 
-	export let isOpen = false
-	export let className = ''
+  export let isOpen = false
+  export let className = ''
 </script>
 
 <button on:click={() => (isOpen = !isOpen)} type="button" class={'flex ' + className}>
-	<img
-		src={downIcon}
-		class={`aspect-square w-[30px] h-[30px] select-none transition-transform duration-300 ${
-			isOpen ? '-rotate-180' : 'rotate-0'
-		}`}
-		alt="Expandir lista"
-	/>
-	{#if isOpen}
-		<div
-			transition:slide
-			class="shadow-lg shadow-[rgba(0,0,0,0.5)] flex flex-col gap-2 py-2 px-4 bg-brand-white border-4 border-[#f0f0f0] rounded-[0.3125rem] absolute translate-y-8 -translate-x-14"
-		>
-			<a
-				href="/profile/me"
-				class="flex items-center justify-center px-1 w-full max-w-[120px] h-[30px] bg-green-700 rounded-[10px] text-white"
-				><b class="font-normal">Mi perfil</b></a
-			>
-			<a
-				href="/"
-				class="flex items-center justify-center px-1 w-full max-w-[120px] h-[30px] bg-green-700 rounded-[10px] text-white"
-				><b class="font-normal">Cerrar sesión</b></a
-			>
-		</div>
-	{/if}
+  <img
+    src={downIcon}
+    class={`aspect-square h-[30px] w-[30px] select-none transition-transform duration-300 ${
+      isOpen ? '-rotate-180' : 'rotate-0'
+    }`}
+    alt="Expandir lista"
+  />
+  {#if isOpen}
+    <div
+      transition:slide
+      class="absolute flex -translate-x-16 translate-y-8 flex-col gap-2 rounded-[0.3125rem] border-4 border-[#f0f0f0] bg-brand-white px-4 py-2 shadow-lg shadow-[rgba(0,0,0,0.5)]"
+    >
+      <a
+        href="/profile/me"
+        class="flex h-[30px] w-full max-w-[150px] items-center justify-center rounded-[10px] bg-green-700 px-1 text-white"
+        ><b class="font-normal">Mi perfil</b></a
+      >
+      <a
+        href="/"
+        class="flex h-[30px] w-full max-w-[150px] items-center justify-center rounded-[10px] bg-green-700 px-1 text-white"
+        ><b class="font-normal">Cerrar sesión</b></a
+      >
+    </div>
+  {/if}
 </button>
 
 <!-- <section class="border-b pb-2">
