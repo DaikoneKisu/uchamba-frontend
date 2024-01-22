f972828b235308e264aa3e71428ce5e8ca29e736
Daikone <eduardoarzolay@gmail.com>
2024-01-22 18:15:41 -0400

    refactor: recover by hand soft and hard skills dropdowns from feature/catalogue.filters

---
+++ src/routes/(navbar)/catalogue/(filters)/HardSkillsDropdown.svelte
@@ -0,0 +1,62 @@
+<script lang="ts">
+  import { flip } from 'svelte/animate'
+  import { fade } from 'svelte/transition'
+  import Chip from '$lib/components/chip/Chip.svelte'
+  import Dropdown from './Dropdown.svelte'
+
+  let hardSkills: string[] = []
+
+  let value = ''
+
+  let input: HTMLInputElement
+
+  function insertHardSkill() {
+    if (value && !hardSkills.includes(value)) {
+      hardSkills = [...hardSkills, value]
+      value = ''
+      input.focus()
+    }
+  }
+</script>
+
+<Dropdown title="Habilidades Duras">
+  <form
+    on:submit={(e) => {
+      e.preventDefault()
+      insertHardSkill()
+    }}
+  >
+    <div class="flex w-full justify-center gap-12">
+      <label
+        class="mt-4 flex h-[49px] w-full max-w-[330px] justify-center rounded-xl border-4 border-[#f0f0f0] bg-brand-white pr-4"
+      >
+        <div class="flex h-full w-full flex-col justify-center pl-5 text-[15px]">
+          <input
+            bind:this={input}
+            list="hard-skills-list"
+            type="text"
+            bind:value
+            placeholder="Ingresa una habilidad dura"
+            class="bg-stone-50 text-sm placeholder:text-brand-p-black focus:border-0 focus:shadow-none focus:outline-none"
+          />
+        </div>
+      </label>
+    </div>
+  </form>
+
+  <div class="w-full">
+    <ul class="flex w-full flex-wrap items-center justify-center gap-2 pt-3">
+      {#each hardSkills as hardSkill (hardSkill)}
+        <div animate:flip in:fade class="flex justify-center">
+          <Chip
+            key={hardSkill}
+            text={hardSkill}
+            deleteHandler={() => {
+              alert('se borró la habilidad dura ')
+            }}
+          />
+        </div>
+      {/each}
+    </ul>
+  </div>
+</Dropdown>
